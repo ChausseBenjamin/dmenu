8116f551e3d98727e81199a908260021dc1d096e
Benjamin Chausse <benjamin@chausse.xyz>
2020-10-15 17:06:19 -0400

    Working centered dmenu

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Luke's dmenu
+# My dmenu
 
 Extra stuff added to vanilla dmenu:
 
@@ -7,6 +7,7 @@ Extra stuff added to vanilla dmenu:
 - can view color characters like emoji (libxft-bgra is required for this reason)
 - `-P` for password mode: hide user input
 - `-r` to reject non-matching input
+- `-c` to reject non-matching input
 - dmenu options are mouse clickable
 
 ## Installation
--- config.def.h
+++ config.def.h
@@ -2,10 +2,12 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
+	"monospace:size=12",
+	"JoyPixels:pixelsize=11:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
@@ -24,7 +26,7 @@ static const unsigned int alphas[SchemeLast][2] = {
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 11;
 
 /*
  * Characters not considered part of a word while deleting words
